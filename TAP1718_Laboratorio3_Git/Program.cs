00abf7b9f6ba7a90968b886b408dff734303c5a9
Luca Demetrio <luca.demetrio@outlook.it>
2017-11-06 23:17:06 +0100

    Prima commit!
    Questa è la prima commit del progetto.

---
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TAP1718_Laboratorio3_Git
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
